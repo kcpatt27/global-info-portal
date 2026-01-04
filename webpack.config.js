51fdd8f5a0ee93199e8ac8ad0341bf4735ffdbcc
kcpatt27 <kcpatterson27@gmail.com>
2026-01-04 06:47:28 -0500

    Bundle all CSS files and fix GitHub Pages deployment

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = (env = {}) => {
       filename: isProd ? '[name].[contenthash].js' : '[name].js',
       chunkFilename: isProd ? '[name].[contenthash].chunk.js' : '[name].chunk.js',
       clean: true,
-      publicPath: '/dist/',
+      publicPath: '/global-info-portal/dist/',
     },
     optimization: {
       moduleIds: 'deterministic',
