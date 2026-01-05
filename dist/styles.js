9a1125acf5012e19eead4754329858280434bc72
kcpatt27 <kcpatterson27@gmail.com>
2026-01-05 07:20:54 -0500

    webpack rebuild plus minor updates/fixes

--- dist/styles.js
+++ dist/styles.js
@@ -7,12 +7,9 @@
  * or disable the default devtool with "devtool: false".
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
-(self["webpackChunkglobal_information_portal"] = self["webpackChunkglobal_information_portal"] || []).push([["styles"],{
+(self["webpackChunkglobal_information_portal"] = self["webpackChunkglobal_information_portal"] || []).push([[869],{
 
 /***/ 8934:
-/*!*****************************!*\
-  !*** ./css-styles/main.css ***!
-  \*****************************/
 /***/ (function() {
 
 eval("{// extracted by mini-css-extract-plugin\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiODkzNC5qcyIsIm1hcHBpbmdzIjoiQUFBQSIsInNvdXJjZXMiOlsid2VicGFjazovL2dsb2JhbC1pbmZvcm1hdGlvbi1wb3J0YWwvLi9jc3Mtc3R5bGVzL21haW4uY3NzP2U2ZjUiXSwic291cmNlc0NvbnRlbnQiOlsiLy8gZXh0cmFjdGVkIGJ5IG1pbmktY3NzLWV4dHJhY3QtcGx1Z2luXG5leHBvcnQge307Il0sIm5hbWVzIjpbXSwiaWdub3JlTGlzdCI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///8934\n\n}");
